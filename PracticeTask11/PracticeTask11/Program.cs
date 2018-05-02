ddf57390023f0497116f53b61f8b73837940b838
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 17:33:46 +0500

    Algorithm developed.

--- PracticeTask11/PracticeTask11/Program.cs
+++ PracticeTask11/PracticeTask11/Program.cs
@@ -12,7 +12,40 @@ namespace PracticeTask11
         // Student: Alexey Subbotin. Group: SE-17-1.
         static void Main(string[] args)
         {
-            
+            // Receiving the message,
+            string inputMessage = Console.ReadLine();
+
+            // Decrypted message.
+            string realMessage = "";
+
+            // Decrypting the message.
+            for(int i = 0; i < inputMessage.Length; i += 3)
+            {
+                // The number of zeros in a group of 3.
+                int zeroCount = 0;
+
+                // The number of ones in a group of 3.
+                int oneCount = 0;
+
+                // Counting ones and zeros.
+                for (int j = i; j < i + 3; j++)
+                {
+                    if (inputMessage[i] == '0')
+                        zeroCount++;
+                    else
+                        oneCount++;
+                }
+
+                // Adding to the decrypted the most frequent symbol.
+                if (zeroCount > oneCount)
+                    realMessage += "0";
+                else
+                    realMessage += "1";
+            }
+
+            Console.WriteLine(realMessage);
+
+            Console.ReadLine();
         }
     }
 }
