839a46a88f46110abf302eb05b7206365af48675
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 17:26:24 +0500

    initial commit

---
+++ PracticeTask11/PracticeTask11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask11
+{
+    class Program
+    {
+        // Task: decrypt binary message each symbol of which occurs 3 times (111000111 decrypts as 101).
+        // Student: Alexey Subbotin. Group: SE-17-1.
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
